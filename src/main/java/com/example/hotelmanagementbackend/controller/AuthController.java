3a369ec9f8a18738511e3e2125f24756e86c8dcd
Imene-Amirat <imene2004.amirat@gmail.com>
2025-04-27 19:13:01 +0100

    Creating and returning delete-cookie

--- src/main/java/com/example/hotelmanagementbackend/controller/AuthController.java
+++ src/main/java/com/example/hotelmanagementbackend/controller/AuthController.java
@@ -4,6 +4,7 @@ import com.example.hotelmanagementbackend.dto.LoginRequest;
 import com.example.hotelmanagementbackend.model.User;
 import com.example.hotelmanagementbackend.service.AuthService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,24 +26,18 @@ public class AuthController {
         User user = authService.login(loginRequest);
         //create session and store user
         request.getSession().setAttribute("user", user);
-
-
         return ResponseEntity.ok(Map.of("message", "Login successful"));
     }
 
     @GetMapping("/isAuthenticated")
-    public ResponseEntity<Map<String,Object>> isAuth(HttpServletRequest request){
-        User user = (User) request.getSession().getAttribute("user");
-
-        if(user != null) {
-            return ResponseEntity.ok(Map.of("loggedIn", true));
-        }
-        else{
-            return ResponseEntity.ok(Map.of("loggedIn", false));
-        }
+    public ResponseEntity<Map<String, Object>> isAuth(HttpServletRequest request) {
+        boolean loggedIn = authService.isAuth(request);
+        return ResponseEntity.ok(Map.of("loggedIn", loggedIn));
     }
 
-
-
-
+    @PostMapping("/logout")
+    public ResponseEntity<Map<String, String>> logout(HttpServletRequest request, HttpServletResponse response) {
+        authService.logout(request,response);
+        return ResponseEntity.ok(Map.of("message", "Logged out"));
+    }
 }
--- src/main/java/com/example/hotelmanagementbackend/service/AuthService.java
+++ src/main/java/com/example/hotelmanagementbackend/service/AuthService.java
@@ -5,6 +5,10 @@ import com.example.hotelmanagementbackend.exception.InvalidPasswordException;
 import com.example.hotelmanagementbackend.exception.UserNotFoundException;
 import com.example.hotelmanagementbackend.model.User;
 import com.example.hotelmanagementbackend.repository.UserRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -29,5 +33,30 @@ public class AuthService {
         }
 
         return userFound.get();
+    }
+
+    public boolean isAuth(HttpServletRequest request){
+        HttpSession session = request.getSession(false);
+        if(session != null && session.isNew()){
+            User user = (User) session.getAttribute("user");
+            return user != null;
+        }
+        return false;
+    }
+
+    public void logout(HttpServletRequest request, HttpServletResponse response){
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+
+        //delete the JSESSIONID cookie manually
+        Cookie cookie = new Cookie("JSESSIONID", null);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(0);
+        cookie.setSecure(false);
+
+        response.addCookie(cookie);
     }
 }
