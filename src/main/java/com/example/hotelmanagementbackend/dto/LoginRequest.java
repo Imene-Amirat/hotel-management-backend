0b5cdbf422baabf75acfdd32e9c314a3c345c835
Imene-Amirat <imene2004.amirat@gmail.com>
2025-04-30 10:38:06 +0100

    add validation annotations

--- src/main/java/com/example/hotelmanagementbackend/dto/LoginRequest.java
+++ src/main/java/com/example/hotelmanagementbackend/dto/LoginRequest.java
@@ -1,5 +1,7 @@
 package com.example.hotelmanagementbackend.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Getter
@@ -7,7 +9,11 @@ import lombok.*;
 @NoArgsConstructor
 @ToString
 public class LoginRequest {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+
+    @NotBlank(message = "Password is required")
     private String password;
 
     public LoginRequest(String email, String password) {
--- src/main/java/com/example/hotelmanagementbackend/dto/RegisterRequest.java
+++ src/main/java/com/example/hotelmanagementbackend/dto/RegisterRequest.java
@@ -1,5 +1,8 @@
 package com.example.hotelmanagementbackend.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -9,7 +12,14 @@ import lombok.*;
 @ToString
 
 public class RegisterRequest {
+    @NotBlank(message = "Username is required")
     private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private  String password;
 }
