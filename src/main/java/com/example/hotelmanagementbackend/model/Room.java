cbe5a57f32eecbffacb69f5c9718bc97d4b7fec8
Imene-Amirat <imene2004.amirat@gmail.com>
2025-05-03 23:11:15 +0100

    add some features inside the Room entity.

--- src/main/java/com/example/hotelmanagementbackend/model/Room.java
+++ src/main/java/com/example/hotelmanagementbackend/model/Room.java
@@ -32,4 +32,32 @@ public class Room {
 
     @Column(nullable = false)
     private String imageUrl;
+
+    // NEW FEATURES
+    @Column(nullable = false)
+    private boolean hasWifi;
+
+    @Column(nullable = false)
+    private boolean hasAirConditioning;
+
+    @Column(nullable = false)
+    private boolean hasTV;
+
+    @Column(nullable = false)
+    private boolean hasBreakfastIncluded;
+
+    @Column(nullable = false)
+    private double roomSize;
+
+    @Column(nullable = false)
+    private String bedType; 
+
+    @Column(nullable = false)
+    private boolean hasBalcony;
+
+    @Column(nullable = false)
+    private boolean hasTerrace;
+
+    @Column(nullable = false)
+    private boolean hasKitchen;
 }
---
+++ src/main/java/com/example/hotelmanagementbackend/model/RoomGalleryImage.java
@@ -0,0 +1,24 @@
+package com.example.hotelmanagementbackend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class RoomGalleryImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id", nullable = false)
+    private Room room;
+
+    @Column(nullable = false)
+    private String imageUrl;
+}
